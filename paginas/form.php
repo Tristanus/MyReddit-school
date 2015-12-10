b53abc8ac753b99c6b614a45f8ffd9d20884b68c
Tristanus <tristan_98@hotmail.nl>
2015-12-10 14:15:36 +0100

    MAN!!!! BIG UPDATE!

--- paginas/form.php
+++ paginas/form.php
@@ -4,13 +4,68 @@
 <head>
     <title>My reddit</title>
     <link href="style.css" rel="stylesheet" type="text/css">
+    
+    <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+<script type="text/javascript" src="js/jquery.validate.min.js"></script>
+
+    <script>
+  (function($,W,D)
+{
+    var JQUERY4U = {};
+    JQUERY4U.UTIL =
+    {
+        setupFormValidation: function()
+        {
+            //form validation rules
+            $("#myform").validate({
+                rules: {
+                    author: "required",
+                    title: {
+                        required: true,
+                    minlength: 10
+                    },
+                    url: {
+                        required: true,
+                        url: true
+                    },
+                    summary: {
+                        required: true,
+                        minlength: 100
+                    },
+                },
+                messages: {
+                    author: "Please enter your username",
+                    title: {
+                        required: "Please provide a title",
+                        minlength: "Your title must be at least 10 characters long"
+                    },
+                    url: {
+                        required: "Please provide a url",
+                        url2: "please give a valid url"
+                    },
+                    summary: {
+                        required: "Please provide a summary",
+                        minlength: "The summary must be aleast 100 characters long"
+                    },
+                  
+                },
+                submitHandler: function(form) {
+                    form.submit();
+                }
+            });
+        }
+    }
+    //when the dom has loaded setup form validation rules
+    $(D).ready(function($) {
+        JQUERY4U.UTIL.setupFormValidation();
+    });
+})(jQuery, window, document);
+    
+    </script>
 </head>
 
 <body>
     
-<img src="../afbeeldingen/reddit.png">
-    
-<div id="content">
 <?php
   $connection = mysqli_connect('localhost', 'root');
 	
@@ -24,52 +79,57 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
        
    }else{
        $author = $_POST["author"];
+       $author = mysqli_real_escape_string($connection,$author);
    }
    if (empty($_POST["title"])) {
      $titleErr = "Title is required";
+     $title = mysqli_real_escape_string($connection,$title);   
    } else{
        $title = $_POST["title"];
    }
    
   if (empty($_POST["url"])) {
      $urlErr = "URL is required";
+     $url = mysqli_real_escape_string($connection,$url);
    } else{
        $url = $_POST["url"];
   }
    
    if (empty($_POST["summary"])) {
      $summaryErr = "Summary is required";
+     $summary = mysqli_real_escape_string($connection,$summary);   
    }else{
        $summary = $_POST["summary"];
    }
 }
    
-?>
+?>   
+    
+<img src="../afbeeldingen/reddit.png">
 
 <div class="content">
 <h2>Artikel toevoegen...</h2>
     <a href="http://localhost/Werkbonnen/Werkbon%201/paginas/"><img class="homeform" src="../afbeeldingen/home.png"></a>
 <p class="text"><span class="error">* = required field.</span></p>
-<form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
+<form id="myform" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> 
     <p class="text">
    Username: <input class="username" type="text" name="author" value="">
-   <span class="error">* <?php echo $authorErr;?></span>
    <br><br>
    Title: <input type="text" name="title" value="">
-   <span class="error">* <?php echo $titleErr;?></span>
    <br><br>
    URL: <input type="text" name="url" value="">
-   <span class="error">* <?php echo $urlErr;?></span>
    <br><br>
    Summary: <textarea class="sum" name="summary" rows="6" cols="50"></textarea>
-   <span class="error">* <?php echo $summaryErr;?></span>
    <br><br>
     <input type="submit" name="submit" value="Submit"> 
     </p>    
 </form>
+    
+    
 
 
-<?php
+<?php 
+    
  if (isset($_POST['submit'])){
    $required = array('author', 'title', 'url', 'summary');
 // Loop over field names, make sure each one exists and is not empty
